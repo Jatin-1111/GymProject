27055295a064acd85bcc1eed95a65548956dd4ca
Mr-Tracko <shubhamgupta57019@gmail.com>
2025-08-29 11:50:29 +0530

    changed tailwind ig

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,15 +1,6 @@
-// const config = {
-//   plugins: {
-//     "@tailwindcss/postcss": {},
-//   },
-// };
-
-// export default config;
-
 const config = {
   plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+    "@tailwindcss/postcss": {},
   },
 };
 
